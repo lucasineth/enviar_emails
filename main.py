d941ea3be66c438724481315f0d6bad378e86dc0
lucasineth <lucasramos1418@gmail.com>
2024-12-10 11:01:45 -0300

    feat: email body to send to customer

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ import smtplib
 clientes = pd.read_excel('./clientes.xlsx')
 
 for index, cliente in clientes.iterrows():
-    print(cliente['nome'], cliente['email'])
+    msg = MIMEMultipart()
+    msg['subject'] = 'Email do Lucas Ramos :)'
+    msg['from'] = 'lucasramos1418@gmail.com'
+    msg['to'] = cliente['email']
+    message = f"Olá {cliente['nome']}, você recebeu um email teste do lucasdev"
+    msg.attach(MIMEText(message, 'plain'))
+    
+    
+    
     
